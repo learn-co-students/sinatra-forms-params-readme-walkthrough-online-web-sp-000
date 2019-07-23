157e341f05ae9fe7b698b3183a7914b462b45edf
Donna Amos <donnacamos.developer@gmail.com>
2019-07-23 01:41:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/food_form' do
+  
+   get '/' do
     erb :food_form
   end
 
+
   # Add your post route and action below
-  post '/food' do 
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-  end 
+  post '/food' do
+  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 end
