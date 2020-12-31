c3281231f794898a594ed5d4d9e14848d15f24bb
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-12-31 12:30:08 -0600

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  # Add your post route and action below
-
+  # Add your post route and action below 
+  # notice that both of the attributes from the form are covered in this route: The method post and the action /food. It's almost like a game of catch - the form is throwing the data to the server, which catches it by having the same receiving address /food and way of receiving the data (post).
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
 end
